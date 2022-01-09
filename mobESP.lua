d89b5a89b1e0011f54957948c850dcc20f341797
hahahahz1 <newdisc123@protonmail.com>
2022-01-09 09:51:09 -0500

    d

--- mobESP.lua
+++ mobESP.lua
@@ -54,15 +54,47 @@ for _, mob in ipairs(getMobs()) do
     local function boxesp()
         local connection 
         connection = game:GetService("RunService").RenderStepped:Connect(function()
-            if mob.Parent ~= nil and mob:FindFirstChildOfClass("Humanoid") then
-                local vector, onScren = workspace.CurrentCamera:WorldToViewportPoint
+            if mob.Parent ~= nil and mob:FindFirstChildOfClass("Humanoid") and mob:FindFirstDescendant("HumanoidRootPart") then
+                local vector, onScren = workspace.CurrentCamera:WorldToViewportPoint(mob:FindFirstDescendant("HumanoidRootPart").Position)
+
+                local rootPart = mob:FindFirstDescendant("HumanoidRootPart")
+                local head = mob:FindFirstDescendant("Head")
+                local headPosition = workspace.CurrentCamera:WorldToViewportPoint(head.Position + Vector3.new(0, 0.5, 0))
+                local LegPosition = workspace.CurrentCamera:WorldToViewportPoint(rootPart - Vector3.new(0,3,0))
+
+                BoxOutline.Size = Vector2.new(1000/ vector.Z, headPosition.Y - LegPosition.Y)
+                BoxOutline.Position = Vector2.new(vector.X - BoxOutline.Size.X / 2, vector.Y - BoxOutline.Size.Y / 2)
+                BoxOutline.Visible = true 
+
+                Box.Size = Vector2.new(1000 / vector.Z, headPosition.Y - LegPosition)
+                Box.Position = Vector2.new(vector.X - Box.Size.X/  2, vector.Y - Box.Size.Y / 2)
+                Box.Visible = true 
+
+                HealthBarOutline.Size = Vector2.new(2, headPosition.Y - LegPosition.Y)
+                HealthBarOutline.Posiiton = BoxOutline.Position - Vector2.new(6, 0)
+                HealthBarOutline.Visible = true 
+
+                HealthBar.Size = Vector2.new(2, (headPosition.Y - LegPosition.Y) / (mob:FindFirstChildOfClass("Humanoid").MaxHealth / math.clamp(mob:FindFirstChildOfClass("Humanoid").Health, 0, mob:FindFirstChildOfClass("Humanoid").MaxHealth)))
+                HealthBar.Position = Vector2.new(Box.Position.X - 6, Box.Position.Y + (1 / HealthBar.Size.Y))
+                HealthBar.Color = Color3.fromHSV(tick() % 5 / 5, 1, 1)
+            else
+                HealthBar.Visible = false 
+                Box.Visible = false 
+                HealthBarOutline.Visible = false 
+                BoxOutline.Visible = false
             end
         end)
 
         if (mob:FindFirstChildOfClass("Humanoid")) then
             mob:FindFirstChildOfClass("Humanoid").Died:Connect(function()
+                HealthBar.Visible = false 
+                Box.Visible = false 
+                HealthBarOutline.Visible = false 
+                BoxOutline.Visible = false
                 connection:Disconnect()
             end)
         end
     end
+
+    boxesp()
 end
