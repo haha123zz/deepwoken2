17a9bb0d4c9ef5021caffdcfb5a4b6dc651eed19
hahahahz1 <newdisc123@protonmail.com>
2022-01-09 09:39:50 -0500

    dd

--- mobESP.lua
+++ mobESP.lua
@@ -23,3 +23,46 @@ local function getMobs()
     return mobs 
 end
 
+for _, mob in ipairs(getMobs()) do
+    local BoxOutline = Drawing.new("Square")
+    BoxOutline.Visible = false 
+    BoxOutline.Color = Color3.new(0,0,0)
+    BoxOutline.Thickness = 3
+    BoxOutline.Transparency = 1
+    BoxOutline.Filled = false
+
+    local Box = Drawing.new("Square")
+    Box.Visible = false
+    Box.Color = Color3.new(1,1,1)
+    Box.Thickness = 1
+    Box.Transparency = 1
+    Box.Filled = false
+
+    local HealthBarOutline = Drawing.new("Square")
+    HealthBarOutline.Thickness = 3
+    HealthBarOutline.Filled = false
+    HealthBarOutline.Color = Color3.new(0,0,0)
+    HealthBarOutline.Transparency = 1
+    HealthBarOutline.Visible = false
+
+    local HealthBar = Drawing.new("Square")
+    HealthBar.Thickness = 1
+    HealthBar.Filled = false
+    HealthBar.Transparency = 1
+    HealthBar.Visible = false
+
+    local function boxesp()
+        local connection 
+        connection = game:GetService("RunService").RenderStepped:Connect(function()
+            if mob.Parent ~= nil and mob:FindFirstChildOfClass("Humanoid") then
+                local vector, onScren = workspace.CurrentCamera:WorldToViewportPoint
+            end
+        end)
+
+        if (mob:FindFirstChildOfClass("Humanoid")) then
+            mob:FindFirstChildOfClass("Humanoid").Died:Connect(function()
+                connection:Disconnect()
+            end)
+        end
+    end
+end
